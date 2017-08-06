7204b19bd5527a40e367e33cc5c4fa2f84f44512
Nick Rodriguez <endotnick@gmail.com>
2017-08-06 20:41:27 +0000

    add solution

---
+++ stock_picker.rb
@@ -0,0 +1,17 @@
+def stock_picker set
+    profit = 0
+    for i in 0...set.length
+        for j in (i+1)...set.length
+            if ((set[j] - set[i]) > profit)
+                    profit = set[j] - set[i]
+                    buy = i
+                    sell = j
+            end
+        end
+    end
+    days = [buy, sell]  
+    puts days
+    puts profit
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
